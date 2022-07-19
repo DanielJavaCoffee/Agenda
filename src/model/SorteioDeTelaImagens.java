08e81cb53e545f9b9fbe22b13d785a51d2bcd89d
Daniel Silva <danielsilvacr123@gmail.com>
2022-07-19 15:26:00 -0300

    pasta

--- src/model/SorteioDeTelaImagens.java
+++ src/model/SorteioDeTelaImagens.java
@@ -6,8 +6,8 @@ public class SorteioDeTelaImagens {
 
 	public String sorteioDeTela() {
 		
-			String imagens[] = { "src/Imagens/pipocaFilme.jpeg", "src/Imagens/imagensDEFilmes.png",
-					"src/Imagens/xx.png" };
+			String imagens[] = { "FundoDeTela/pipocaFilme.jpeg", "FundoDeTela/imagensDEFilmes.png",
+					"FundoDeTela/foto01.png" };
 
 			int numero = new Random().nextInt(3);
 			String sorteio = imagens[numero];
